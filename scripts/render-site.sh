e76e4ee603f5ba93d00f486915a79066bd44ce57
astro <github.u3vwv@passfwd.com>
2026-07-25 23:15:48 -0300

    Refine GitHub links (#12)

--- scripts/render-site.sh
+++ scripts/render-site.sh
@@ -44,11 +44,20 @@ render_app_card()
   cat <<EOF
           <article class="card app-card">
             <div>
-              <h2>$app_name</h2>
+              <div class="app-heading">
+                <h2>$app_name</h2>
+                <a class="github-link" href="$repository_url">
+                  <svg
+                    class="github-icon"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path d="M10.226 17.284c-2.965-.36-5.054-2.493-5.054-5.256 0-1.123.404-2.336 1.078-3.144-.292-.741-.247-2.314.09-2.965.898-.112 2.111.36 2.83 1.01.853-.269 1.752-.404 2.853-.404 1.1 0 1.999.135 2.807.382.696-.629 1.932-1.1 2.83-.988.315.606.36 2.179.067 2.942.72.854 1.101 2 1.101 3.167 0 2.763-2.089 4.852-5.098 5.234.763.494 1.28 1.572 1.28 2.807v2.336c0 .674.561 1.056 1.235.786 4.066-1.55 7.255-5.615 7.255-10.646C23.5 6.188 18.334 1 11.978 1 5.62 1 .5 6.188.5 12.545c0 4.986 3.167 9.12 7.435 10.669.606.225 1.19-.18 1.19-.786V20.63a2.9 2.9 0 0 1-1.078.224c-1.483 0-2.359-.808-2.987-2.313-.247-.607-.517-.966-1.034-1.033-.27-.023-.359-.135-.359-.27 0-.27.45-.471.898-.471.652 0 1.213.404 1.797 1.235.45.651.921.943 1.483.943.561 0 .92-.202 1.437-.719.382-.381.674-.718.944-.943"/>
+                  </svg>
+                  <span>GitHub</span>
+                </a>
+              </div>
               <p>$app_summary</p>
-              <a class="source-link" href="$repository_url">
-                View on GitHub
-              </a>
             </div>
             <div class="actions">
               <a
@@ -108,6 +117,7 @@ render_app_files()
   sed \
     -e "s|@APP_ID@|$(escape_sed_replacement "$app_id")|g" \
     -e "s|@APP_NAME@|$(escape_sed_replacement "$(app_html_value "$repository" name)")|g" \
+    -e "s|@REPOSITORY_URL@|$(escape_sed_replacement "https://github.com/$repository")|g" \
     "$repository_root/templates/app-install.html" \
     > "$install_directory/index.html"
 }
